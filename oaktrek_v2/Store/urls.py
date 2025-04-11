3ece4fc7bb65ff63b1cab07344c7c78c33cb5259
Rachit <rachitgoyal1410@gmail.com>
2025-04-11 18:30:30 +0530

    loaded up the index page(finally!!!!)

--- oaktrek_v2/Store/urls.py
+++ oaktrek_v2/Store/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('products/<str:collection_name>/', views.products_view, 
+    name='products'),
+    path('products/<str:collection_name>/<slug:product_slug>/', views.product_page_view, name='product_page'),
     path('stores/', views.stores, name='stores'),
+    path('coming_soon/', views.coming_soon, name='coming_soon'),
     path('sustainability/', views.sustainability, name='sustainability'),
 ]
--- oaktrek_v2/Store/views.py
+++ oaktrek_v2/Store/views.py
@@ -12,4 +12,22 @@ def sustainability(request):
 def stores(request):
     return render(request, 'stores.html')
 
-    
+def coming_soon(request):
+    return render(request, "coming_soon.html")
+
+def products_view(request, collection_name):
+    context = {
+        'collection_name': collection_name,
+    }
+    return render(request, 'products.html', context)
+
+
+
+from django.shortcuts import render
+
+def product_page_view(request, collection_name, product_slug):
+    context = {
+        'collection_name': collection_name,
+        'product_slug': product_slug,
+    }
+    return render(request, 'product_page.html', context)
